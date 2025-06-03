348bbbaef609e7322ac18f5400c327a1f54a51d5
yanun0323 <yanun0323@gmail.com>
2025-06-03 18:02:47 +0800

    feat: provide Reset color

--- colorize.go
+++ colorize.go
@@ -5,47 +5,46 @@ import (
 	"sync"
 )
 
-var (
-	_colorPrefix = "\x1b["
-	_colorReset  = "\x1b[0m"
+const (
+	Reset = "\x1b[0m"
 )
 
 const (
-	Black   = "30m"
-	Red     = "31m"
-	Green   = "32m"
-	Yellow  = "33m"
-	Blue    = "34m"
-	Magenta = "35m"
-	Cyan    = "36m"
-	White   = "37m"
+	Black   = "\x1b[30m"
+	Red     = "\x1b[31m"
+	Green   = "\x1b[32m"
+	Yellow  = "\x1b[33m"
+	Blue    = "\x1b[34m"
+	Magenta = "\x1b[35m"
+	Cyan    = "\x1b[36m"
+	White   = "\x1b[37m"
 
-	BlackReversed   = "40m"
-	RedReversed     = "41m"
-	GreenReversed   = "42m"
-	YellowReversed  = "43m"
-	BlueReversed    = "44m"
-	MagentaReversed = "45m"
-	CyanReversed    = "46m"
-	WhiteReversed   = "47m"
+	BlackReversed   = "\x1b[40m"
+	RedReversed     = "\x1b[41m"
+	GreenReversed   = "\x1b[42m"
+	YellowReversed  = "\x1b[43m"
+	BlueReversed    = "\x1b[44m"
+	MagentaReversed = "\x1b[45m"
+	CyanReversed    = "\x1b[46m"
+	WhiteReversed   = "\x1b[47m"
 
-	BrightBlack   = "90m"
-	BrightRed     = "91m"
-	BrightGreen   = "92m"
-	BrightYellow  = "93m"
-	BrightBlue    = "94m"
-	BrightMagenta = "95m"
-	BrightCyan    = "96m"
-	BrightWhite   = "97m"
+	BrightBlack   = "\x1b[90m"
+	BrightRed     = "\x1b[91m"
+	BrightGreen   = "\x1b[92m"
+	BrightYellow  = "\x1b[93m"
+	BrightBlue    = "\x1b[94m"
+	BrightMagenta = "\x1b[95m"
+	BrightCyan    = "\x1b[96m"
+	BrightWhite   = "\x1b[97m"
 
-	BrightBlackReversed   = "100m"
-	BrightRedReversed     = "101m"
-	BrightGreenReversed   = "102m"
-	BrightYellowReversed  = "103m"
-	BrightBlueReversed    = "104m"
-	BrightMagentaReversed = "105m"
-	BrightCyanReversed    = "106m"
-	BrightWhiteReversed   = "107m"
+	BrightBlackReversed   = "\x1b[100m"
+	BrightRedReversed     = "\x1b[101m"
+	BrightGreenReversed   = "\x1b[102m"
+	BrightYellowReversed  = "\x1b[103m"
+	BrightBlueReversed    = "\x1b[104m"
+	BrightMagentaReversed = "\x1b[105m"
+	BrightCyanReversed    = "\x1b[106m"
+	BrightWhiteReversed   = "\x1b[107m"
 )
 
 var (
@@ -61,7 +60,7 @@ func String(s string, c string) string {
 	buf := bufferPool.Get().(*bytes.Buffer)
 	defer bufferPool.Put(buf)
 	buf.Reset()
-	buf.Grow(len(s) + len(_colorPrefix) + len(c) + len(_colorReset))
+	buf.Grow(len(s) + len(c) + len(Reset))
 	Write(buf, s, c)
 	return buf.String()
 }
@@ -71,7 +70,7 @@ func Bytes(s []byte, c string) []byte {
 	buf := bufferPool.Get().(*bytes.Buffer)
 	defer bufferPool.Put(buf)
 	buf.Reset()
-	buf.Grow(len(s) + len(_colorPrefix) + len(c) + len(_colorReset))
+	buf.Grow(len(s) + len(c) + len(Reset))
 	Write(buf, string(s), c)
 	return buf.Bytes()
 }
@@ -83,8 +82,7 @@ type StringWriter interface {
 
 // Write write colorized string to buffer
 func Write(buf StringWriter, s string, c string) {
-	buf.WriteString(_colorPrefix)
 	buf.WriteString(c)
 	buf.WriteString(s)
-	buf.WriteString(_colorReset)
+	buf.WriteString(Reset)
 }
